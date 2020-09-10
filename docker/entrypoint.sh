25dc247c1be07d5300e459d3f89c73d6561f9057
wm-efren-carles <efrencalres@worldteamproject.com>
2020-09-10 11:35:29 +0200

    Works

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-if [[ -z "$INPUT_AWS_ACCESS_KEY_ID" ]]
+if [[ -z "$AWS_ACCESS_KEY_ID" ]]
 then
     echo "AWS_ACCESS_KEY_ID is missing"
     exit 1
 fi
 
-if [[ -z "$INPUT_AWS_SECRET_ACCESS_KEY" ]]
+if [[ -z "$AWS_SECRET_ACCESS_KEY" ]]
 then
     echo "AWS_SECRET_ACCESS_KEY is missing"
     exit 1
 fi
 
-if [[ -z "$INPUT_AWS_REGION" ]]
+if [[ -z "$AWS_DEFAULT_REGION" ]]
 then
     echo "AWS_REGION is missing"
     exit 1
@@ -38,10 +38,6 @@ then
 fi
 
 ## Set AWS credentials
-export AWS_ACCESS_KEY_ID="${INPUT_AWS_ACCESS_KEY_ID}"
-export AWS_SECRET_ACCESS_KEY="${INPUT_AWS_SECRET_ACCESS_KEY}"
-export AWS_DEFAULT_REGION="${INPUT_AWS_REGION}"
-
 ServiceName=`aws ecs list-services --cluster "${INPUT_CLUSTER_NAME}" | jq '.serviceArns[] | select(. | contains( "'${INPUT_SERVICE_NAME}'" ))' | cut -d "\"" -f 2 `
 ServiceImageCoincidence=`aws ecs list-tasks --cluster "${INPUT_CLUSTER_NAME}" --service-name "${ServiceName}" | jq '.taskArns[] | select(. | contains( "'${INPUT_CURRENT_IMAGE}'" ) | not)'`
 if [ -z "${ServiceImageCoincidence}" ]
